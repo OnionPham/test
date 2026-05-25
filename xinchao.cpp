22d606dd55396bea174980c8657f386a4621cf41
OnionPham <Cuhanh0908@gmail.com>
2026-05-25 12:27:33 +0700

    50A

--- xinchao.cpp
+++ xinchao.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int x,y,z;
-    cin >> x >> y >> z;
-    long long a = sqrt((x*z) / y);
-    long long b = sqrt((y*z) / x);
-    long long c = sqrt((x*y) / z);
-    cout << 4*(a+b+c) << endl;
+    int x,y;
+    cin >> x >> y;
+    long long Tong = x * y;
+    int domino = 2, dem = 0;
+    while(Tong >= domino){//khi nao ma domino == Tong thi dung
+        Tong -= domino;
+        dem++;
+    }
+    cout << dem;
 }
