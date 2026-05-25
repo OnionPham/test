5fcfad7f4c23ec34eb85fa593dd313d43009489c
OnionPham <Cuhanh0908@gmail.com>
2026-05-25 12:31:43 +0700

    xin chao

--- xinchao.cpp
+++ xinchao.cpp
@@ -3,11 +3,5 @@ using namespace std;
 int main(){
     int x,y;
     cin >> x >> y;
-    long long Tong = x * y;
-    int domino = 2, dem = 0;
-    while(Tong >= domino){//khi nao ma domino == Tong thi dung
-        Tong -= domino;
-        dem++;
-    }
-    cout << dem;
+    cout << (x * y) / 2 << endl;
 }
