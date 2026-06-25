c44d17e096ccec348ec23ca26a88e9344a1c2262
OnionPham <Cuhanh0908@gmail.com>
2026-06-25 11:00:46 +0700

    Add xinchao.cpp

--- Codce C++/xinchao.cpp
+++ Codce C++/xinchao.cpp
@@ -9,12 +9,20 @@ public:
     string mergeAlternately(string word1,string word2){
         int w1 = 0,w2=0;
         string res = "";
-        while(1){
+        while(w1 < word1.size() && w2 < word2.size()){
             res += word1[w1];
             w1++;
             res += word2[w2];
             w2++;
         }
+         while(w1 < word1.size()){
+            res += word1[w1];
+            w1++;
+         }
+         while(w2 < word2.size()){
+            res += word2[w2];
+            w2++;
+         }
         return res;
     }
 };
