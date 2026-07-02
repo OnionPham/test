8c25edf94489de8832b45a18cc3a19f9c0d418fa
OnionPham <Cuhanh0908@gmail.com>
2026-07-02 12:45:58 +0700

    xinchao

--- Codce C++/xinchao.cpp
+++ Codce C++/xinchao.cpp
@@ -3,24 +3,22 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
+#include <stack>
 using namespace std;
 class Solution{
 public:
     vector<int>dailyTemperatures(vector<int> &temperatures){
         int n = temperatures.size();
         vector<int>res(n);
+        stack<int>st;
         for(int i = 0;i<n;i++){
-            int j = i + 1;
-            while(j < n && temperatures[j] <= temperatures[i])j++;
-            res[i] = (j < n) ? j - i : 0;
+            while( !st.empty() && temperatures[i] > temperatures[st.top()]){
+                int idx = st.top();
+                st.pop();
+                res[idx] = i - idx;
+            }
+            
         }
         return res;
     }
 };
-int main(){
-    vector<int>temps = {73, 74, 75, 71, 69, 72, 76, 73};
-    Solution sol;
-    vector<int>result = sol.dailyTemperatures(temps);
-    for(int x : result) cout << x << " ";
-    return 0;
-}
