fadf61164d416c2234d29f862e05b6272a43c42c
OnionPham <Cuhanh0908@gmail.com>
2026-07-02 12:30:06 +0700

    xinchao

--- Codce C++/xinchao.cpp
+++ Codce C++/xinchao.cpp
@@ -6,25 +6,21 @@
 using namespace std;
 class Solution{
 public:
-   int removeDuplicates(vector<int>&nums){
-        string res;
-        int i = 0,j=1;
-        while(i < nums.size() && j < nums.size()){
-            if(nums[i] == nums[j]){
-                j++;
-            }
-            else{
-                res.push_back(nums[i]);
-                i = j;
-                j++;
-            }
+    vector<int>dailyTemperatures(vector<int> &temperatures){
+        int n = temperatures.size();
+        vector<int>res(n);
+        for(int i = 0;i<n;i++){
+            int j = i + 1;
+            while(j < n && temperatures[j] <= temperatures[i])j++;
+            res[i] = (j < n) ? j - i : 0;
         }
-        return res.size();
-   }
+        return res;
+    }
 };
 int main(){
-    vector<int>nums;
-    for(auto &x : nums)cin >> x;
+    vector<int>temps = {73, 74, 75, 71, 69, 72, 76, 73};
     Solution sol;
-    cout << sol.removeDuplicates(nums);
+    vector<int>result = sol.dailyTemperatures(temps);
+    for(int x : result) cout << x << " ";
+    return 0;
 }
