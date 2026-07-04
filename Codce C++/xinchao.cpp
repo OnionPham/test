d3a412889062aacd2e11a845f718872acec24e1e
OnionPham <Cuhanh0908@gmail.com>
2026-07-04 09:58:41 +0700

    xinchao

--- Codce C++/xinchao.cpp
+++ Codce C++/xinchao.cpp
@@ -7,32 +7,32 @@
 using namespace std;
 class Solution{
 public:
-    int evalRPN(vector<string>&tokens){
-        stack<int>st;
-        int ketqua = 0;
-        for(int i = 0;i<tokens.size();i++){
-            if(isdigit(stoi(tokens[i]))){
-                st.push(stoi(tokens[i]));
-            }
-            else if (!isdigit(stoi(tokens[i]))){
-                int a = st.top(),b=st.top();
-                int res;
-                if(tokens[i] == "+"){
-                     res = b + a;//+
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n < 3) return 0;
+
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
                 }
-                else if(tokens[i] == "-"){
-                     res = b - a;//-
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
                 }
-                else if(tokens[i] == "*"){
-                    res = b * a;
-                }
-                else if(tokens[i] == "/"){
-                     res = b / a;
-                }
-                st.push(res);
+                right--;
             }
         }
-        ketqua = st.top();
-        return ketqua;
+
+        return water;
     }
 };
