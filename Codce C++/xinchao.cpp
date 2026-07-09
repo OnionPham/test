f0c87d0f8d210e6607b13c5e850198c14e52ea1c
OnionPham <Cuhanh0908@gmail.com>
2026-07-09 10:35:13 +0700

    xinchao

--- Codce C++/xinchao.cpp
+++ Codce C++/xinchao.cpp
@@ -6,20 +6,23 @@
 #include <stack>
 using namespace std;
 class Solution{
-    public:
-    int largestRectangleArea(vector<int>&heights){
-        int dientich = 0;
-        int n = heights.size();
-        stack<int> st;
-        for (int i = 0; i <= n; i++) {
-            while (!st.empty() && (i == n || heights[st.top()] >= heights[i])) {
-                int height = heights[st.top()];
-                st.pop();
-                int width = st.empty() ? i : i - st.top() - 1;
-                dientich = max(dientich, height * width);
-            }
-            st.push(i);
+public:
+   ListNode* mergeTwoLists(ListNode* list1,ListNode* list2){
+     ListNode* temp = new ListNode(0);
+     ListNode* cur = head;
+     while(list1 != nullptr && list2 != nullptr){
+        if(list1 -> val <= list2->val){
+            cur->next = list1;
+            list1 = list1 -> next;
         }
-        return dientich;
-    }
+        else{
+            cur->next = list2;
+            list2= list2->next;
+        }
+        cur = cur->next;
+     }
+     if(list1 != nullptr)cur -> next = list1;
+     if(list2 != nullptr)cur -> next = list2;
+     return head -> next;
+   }
 }
